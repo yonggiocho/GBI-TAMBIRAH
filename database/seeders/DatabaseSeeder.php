feee8308b16c6db2fccedcc5f3d76ba5cbace082
yonggiocho <yonggikawoco@gmail.com>
2025-12-23 20:05:06 +0700

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Identitas;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Crypt;
@@ -33,8 +34,31 @@ class DatabaseSeeder extends Seeder
             ]
         ];
 
+         $identitas = [
+            'nama_website' => 'Gereja Bethel Indonesia Tumbang Tambirah',
+            'alamat' => 'Jalan Perjuangan Desa Tumbang Tambirah',
+            'telepon' => '-',
+            'email' => 'test@gmail.com',
+            'logo' => '',
+            'favicon' => '',
+            'facebook' => '-',
+            'instagram' => '-',
+            'youtube' => '-',
+            'map' => 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4217.675958789164!2d113.8228246!3d-1.0627945!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dfea1f7d01f0055%3A0xc3996fe4a6b8816e!2sGBI%20tbg%20tambirah!5e1!3m2!1sid!2sid!4v1763366741007!5m2!1sid!2sid'
+        ];
+
         foreach($users as $user) {
              User::create($user);
         }
+
+        Identitas::create($identitas);
+
+        DB::table('visi_misi')->insert([
+            'teks_visi' => 'Menjadi seperti Yesus Kristus dengan menjadi murid, dewasa rohani, hidup Kudus, bersaksi dan memberitakan Injil.',
+            'teks_misi' => 'Untuk mencapai visi, Gereja melaksanakan misi: Memberitakan kabar keselamatan Menjadikan orang percaya murid Kristus Melengkapi orang percaya untuk pekerjaan pelayanan bagi pembangunan Tubuh Kristus Meningkatkan persatuan dan kesatuan Tubuh Kristus',
+        ]);
+
+
+
     }
 }
